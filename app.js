b156a37dee9e407eb6b51c0b9a73a6c1384bc4e0
Lukasz Sobek <lukaszsobek@me.com>
2018-08-07 06:30:04 +0200

    Adds fetching and displaying lat and long

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 "use strict"
 
+const yargs = require("yargs");
 const request = require("request");
 require('dotenv').config()
 
-const url = "https://maps.googleapis.com/maps/api/geocode/json?address=marsza%C5%82kowska%2030%20warszawa"
-+ `&key=${process.env.APIKEY}`;
+const argv = yargs.options({
+    a: {
+        describe: "Address of the location to fetch weather data for",
+        demand: true,
+        alias: "address",
+        string: true
+    }
+})
+.help()
+.alias("help", "h")
+.argv;
 
+const encodedAddress = encodeURIComponent(argv.a);
+const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
+ + `&key=${process.env.APIKEY}`;
 
 request({
     url,
     json: true
 }, (err,response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    if(err) {
+        return console.log("Unable to connect to geolocation server");
+    }
+    if (body.status === "ZERO_RESULTS") {
+        return console.log("No results found");
+    }
+    
+    const location = body.results[0].geometry.location
+    console.log(location.lat, location.lng);
 });
